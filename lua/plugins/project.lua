253bf9723d38334685acaf609a9580620b851b56
Takio Yamaoka <y.takio@gmail.com>
2024-02-05 16:50:11 +0900

    add venv for project.nvim

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -13,7 +13,7 @@ return {
 
     -- All the patterns used to detect root dir, when **"pattern"** is in
     -- detection_methods
-    patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+    patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".venv", "venv" },
 
     -- Table of lsp clients to ignore by name
     -- eg: { "efm", ... }
