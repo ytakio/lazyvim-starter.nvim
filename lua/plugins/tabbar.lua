6d196b18bea806c2db8ece281a4bd4eee1c26186
Takio Yamaoka <y.takio@gmail.com>
2024-04-28 09:56:56 +0900

    add bufferline style

---
+++ lua/plugins/tabbar.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    opts = function(_, opts)
+      opts.options.always_show_bufferline = true
+      opts.options.separator_style = "slope"
+    end,
+  },
+}
