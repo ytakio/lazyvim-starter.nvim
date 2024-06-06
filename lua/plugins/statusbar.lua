afdabb4e71728be70c83a4ea5c3fcf65818338f0
Takio Yamaoka <y.takio@gmail.com>
2024-06-06 09:55:36 +0900

    tentatively disabled venv statusbar

--- lua/plugins/statusbar.lua
+++ lua/plugins/statusbar.lua
@@ -7,16 +7,16 @@ return {
       opts.parents = 5
     end,
   },
-  {
-    "nvim-lualine/lualine.nvim",
-    optional = true,
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_c, 4, {
-        require("lualine.components.venv-selector"),
-        cond = function()
-          return vim.bo.filetype == "python"
-        end,
-      })
-    end,
-  },
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     table.insert(opts.sections.lualine_c, 4, {
+  --       require("lualine.components.venv-selector"),
+  --       cond = function()
+  --         return vim.bo.filetype == "python"
+  --       end,
+  --     })
+  --   end,
+  -- },
 }
