b4bcf7d77e901192964e82279a0d0041b9b2f313
Takio YAMAOKA <y.takio@gmail.com>
2024-08-06 07:54:53 +0900

    add autocmd for c/c++

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,19 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local opt = vim.opt
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("my_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "c", "cpp" },
+  callback = function()
+    vim.b.autoformat = true
+    opt.tabstop = 4
+    opt.shiftwidth = 4
+    opt.expandtab = false
+    opt.diffopt = opt.diffopt + "iwhite"
+  end,
+})
