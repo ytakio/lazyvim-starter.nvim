95766d63de474a243dacbed548b428366a32aaf9
Takio YAMAOKA <y.takio@gmail.com>
2024-12-09 10:39:46 +0900

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 
 if vim.fn.executable("lazygit") == 0 then
-  local version = "0.43.1"
-  local cmd = "curl -Lo lazygit.tar.gz 'https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_"
-    .. version
-    .. "_Linux_x86_64.tar.gz'"
+  local version = "0.44.1"
+  local cmd = "curl -Lo lazygit.tar.gz"
+    .. "'https://github.com/jesseduffield/lazygit/releases/download/v" .. version
+    .. "/lazygit_" .. version .. "_Linux_x86_64.tar.gz'"
   os.execute(cmd)
   os.execute("tar xf lazygit.tar.gz lazygit")
   os.execute("mkdir -p ~/.local/bin/")
