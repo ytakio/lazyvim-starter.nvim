e287cce43525c544713a2b9d515358523f6d7f57
Takio Yamaoka <y.takio@gmail.com>
2025-04-28 15:32:16 +0900

    suppress navic

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,7 +29,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.dial" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
-    { import = "lazyvim.plugins.extras.editor.navic" },
+    -- { import = "lazyvim.plugins.extras.editor.navic" },
     -- languages
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.json" },
