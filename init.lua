8994635a69edecc5fc1b9e8a59dbab2c83ae55bb
Takio Yamaoka <y.takio@gmail.com>
2025-07-25 12:58:41 +0900

    add fzf download script

--- init.lua
+++ init.lua
@@ -1,15 +1,53 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 
 if vim.fn.executable("lazygit") == 0 then
-  local version = "0.44.1"
-  local cmd = "curl -Lo lazygit.tar.gz"
-    .. " 'https://github.com/jesseduffield/lazygit/releases/download/v" .. version
-    .. "/lazygit_" .. version .. "_Linux_x86_64.tar.gz'"
+  local name = "lazygit"
+  local user = "jesseduffield"
+  local ext = ".tar.gz"
+  local version = "0.53.0"
+  local archive = name .. "_" .. version .. "_Linux_x86_64" .. ext
+  local file = name .. ext
+  local cmd = "curl -Lo "
+    .. file
+    .. " 'https://github.com/"
+    .. user
+    .. "/"
+    .. name
+    .. "/releases/download/v"
+    .. version
+    .. "/"
+    .. archive
+    .. "'"
   os.execute(cmd)
-  os.execute("tar xf lazygit.tar.gz lazygit")
+  os.execute("tar xf " .. file .. " " .. name)
   os.execute("mkdir -p ~/.local/bin/")
-  os.execute("mv lazygit ~/.local/bin/")
-  os.execute("rm lazygit.tar.gz")
+  os.execute("mv " .. name .. "  ~/.local/bin/")
+  os.execute("rm " .. file)
+end
+
+if vim.fn.executable("fzf") == 0 then
+  local name = "fzf"
+  local user = "junegunn"
+  local ext = ".tar.gz"
+  local version = "0.64.0"
+  local archive = name .. "-" .. version .. "-linux_amd64" .. ext
+  local file = name .. ext
+  local cmd = "curl -Lo "
+    .. file
+    .. " 'https://github.com/"
+    .. user
+    .. "/"
+    .. name
+    .. "/releases/download/v"
+    .. version
+    .. "/"
+    .. archive
+    .. "'"
+  os.execute(cmd)
+  os.execute("tar xf " .. file .. " " .. name)
+  os.execute("mkdir -p ~/.local/bin/")
+  os.execute("mv " .. name .. "  ~/.local/bin/")
+  os.execute("rm " .. file)
 end
 
 require("config.lazy")
