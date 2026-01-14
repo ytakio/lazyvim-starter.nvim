749cf79a8b1264daf8effc7a9f0c8a393865617e
Takio Yamaoka <y.takio@gmail.com>
2026-01-15 08:10:32 +0900

    typo

--- lua/plugins/statusbar.lua
+++ lua/plugins/statusbar.lua
@@ -2,13 +2,5 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_c, 4, {
-        require("lualine.components.venv-selector"),
-        cond = function()
-          return vim.bo.filetype == "python"
-        end,
-      })
-    end,
   },
 }
